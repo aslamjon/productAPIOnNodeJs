b23c1476839663f978c5f989889a82f5610c91c3
aslamjon ibragimov <aslamjon.ibragimov@gmail.com>
2021-09-05 01:58:53 -0900

    added auth

---
+++ controllers/authController.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/authModel");
+
+async function login(req, res) {
+    const secret = "secret";
+    const { username, password } = req.body;
+    
+    try {
+        const user = await User.findUser(username);
+        
+        if (!user) {
+            res.status(400).send({ message: "Login is incorrect" });
+        }
+
+        const isMatch = password === user.password;
+        if (!isMatch) {
+            res.status(400).send({ message: "Password is incorrect" });
+        }
+
+        const token = jwt.sign({ userId: user.id, username: username }, secret, {
+            expiresIn: "1d",
+        });
+        /* secret - secret for virify
+            expiresIn: "1d"  - token live 1 day. 
+        */ 
+        res.status(200).send({
+            token: token,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    login,
+};
+
+// 1. Find User by username
+// 2. if it exists, compare password with database
+// 3. if comparison is successfull, access granted and generate jwt
--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const app = express();
 const { productRouter } = require('./routes/productRouter')
+const {authRouter} = require('./routes/authRouter');
+const { checkUser } = require('./middlewares/authMiddleware');
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json({extended: true})) // if json come backend then it convert to obj in req.body
-app.use('/api/products', productRouter)
+
+app.use('/api/products', checkUser, productRouter)
+app.use('/auth', authRouter)
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
